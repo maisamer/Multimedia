bec44c70195f85a9f5fa119f1f8efddf2453d9e3
Mai <32622675+maisamer@users.noreply.github.com>
2017-10-13 01:06:22 +0200

    vector quantization
    vector quantization technique implementation

---
+++ Vector_Quantization/src/javaapplication4/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author hermas
+ */
+import java.util.ArrayList;
+
+public class Node {
+	Node left;
+	Node right;
+	ArrayList<double[][]> me=new ArrayList<>();
+	double [][]codebook;
+	public Node()
+	{
+		
+	}
+
+}
